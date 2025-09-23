14789360314a4ab7ab7c4a276a48eb5707310d40
stevenclaudiano <steven.claudiano@route66.com.br>
2025-09-23 11:59:47 -0300

    adicionei mais arquivos

--- var.go
+++ var.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	casa1 := 2.99
+	casa2 := 3.99
+	casa3 := 0.99
 
-	fmt.Println("salveeeee")
-
+	fmt.Println("O preço do leite é R$", casa1)
+	fmt.Println("O preço do ovo é R$", casa2)
+	fmt.Println("O preço do pão é R$", casa3)
 }
