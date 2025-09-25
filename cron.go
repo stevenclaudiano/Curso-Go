c3cfc67453e4c5e9a5386c1b7f7975a6df02bf4a
stevenclaudiano <steven.claudiano@route66.com.br>
2025-09-25 11:19:29 -0300

    ajueste de cronome

--- cron.go
+++ cron.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -11,86 +12,72 @@ import (
 	"time"
 )
 
-const monitoramento = 3
+const monitoramentos = 2
 const delay = 5
 
 func main() {
-	exibIntroducao()
-
-	//registralog("site-falso", false)
-
+	exibeIntroducao()
 	for {
-
-		exibMenu()
-		comando := lecomando()
+		exibeMenu()
+		comando := leComando()
 
 		switch comando {
 		case 1:
 			iniciarMonitoramento()
 		case 2:
+			//Chamando aqui
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
 		default:
 			fmt.Println("Não conheço este comando")
 			os.Exit(-1)
-
 		}
 	}
-
 }
 
-func exibIntroducao() {
-
-	nome := "Steven"
-	versao := 1.0
-	fmt.Println("OLA EU SOU O:", nome)
-	fmt.Println("O programa esta na versão:", versao)
-
+func exibeIntroducao() {
+	nome := "Douglas"
+	versao := 1.2
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
 }
 
-func exibMenu() {
-
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - exibir log")
-	fmt.Println("0 - Sair do Programa")
-
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
 }
 
-func lecomando() int {
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
-	var comandolido int
-	fmt.Scan(&comandolido)
-	fmt.Println("O comando escolhido foi:", comandolido)
-
-	return comandolido
-
+	return comandoLido
 }
-
-// restante do código omitido
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	sites := lesitesdoarquivo()
 
-	for i := 0; i < monitoramento; i++ {
+	sites := leSitesDoArquivo()
+
+	for i := 0; i < monitoramentos; i++ {
 		for i, site := range sites {
 			fmt.Println("Testando site", i, ":", site)
 			testaSite(site)
 		}
 		time.Sleep(delay * time.Second)
 		fmt.Println("")
-
 	}
 
 	fmt.Println("")
 }
 
-// restante do código omitido
-
 func testaSite(site string) {
-
 	resp, err := http.Get(site)
 
 	if err != nil {
@@ -99,14 +86,14 @@ func testaSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
-		registralog(site, true)
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
-		registralog(site, false)
+		registraLog(site, false)
 	}
 }
 
-func lesitesdoarquivo() []string {
+func leSitesDoArquivo() []string {
 
 	var sites []string
 
@@ -126,22 +113,33 @@ func lesitesdoarquivo() []string {
 		if err == io.EOF {
 			break
 		}
+
 	}
 
 	arquivo.Close()
-	return sites
 
+	return sites
 }
 
-func registralog(site string, status bool) {
-	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Ocorreu um erro:", err)
 	}
 
-	arquivo.WriteString(time.Now().Format("02/01/206 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+	fmt.Println(string(arquivo))
+}
+
+func registraLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
-
 }
